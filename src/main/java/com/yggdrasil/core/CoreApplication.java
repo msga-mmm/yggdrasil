8a836fb0cbf5b43ed1909c215917a631f01806af
none <none>
2023-12-30 02:32:15 -0500

    feat(files): add endpoint to delete file

--- src/main/java/com/yggdrasil/core/CoreApplication.java
+++ src/main/java/com/yggdrasil/core/CoreApplication.java
@@ -7,10 +7,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 
 import java.util.List;
 import java.util.LinkedList;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.io.Serializable;
 
 class File implements Serializable {
@@ -27,7 +32,7 @@ class File implements Serializable {
 @RestController
 public class CoreApplication {
 
-  List<File> files = new LinkedList(List.of(
+  List<File> files = new LinkedList<>(List.of(
     new File(1, "file-1.pdf"),
     new File(2, "file-2.pdf"),
     new File(3, "file-3.pdf")
@@ -51,5 +56,19 @@ public class CoreApplication {
   public File createFile(@RequestBody File file) {
     this.files.add(file);
     return file;
+  }
+
+  @DeleteMapping("/files/{id}")
+  public ResponseEntity<Long> deleteFile(@PathVariable Long id) {
+    Iterator<File> filesIterator = this.files.iterator();
+
+    while (filesIterator.hasNext()) {
+        File file = filesIterator.next();
+
+        if (file.id == id)
+            filesIterator.remove();
+    }
+
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
   }
 }
