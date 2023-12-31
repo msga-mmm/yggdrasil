428225368a516048af946e7905aca0ea7c2dd147
none <none>
2023-12-31 01:41:55 -0500

    feat(files): add cors to all the endpoints

--- src/main/java/com/yggdrasil/core/CoreApplication.java
+++ src/main/java/com/yggdrasil/core/CoreApplication.java
@@ -2,17 +2,19 @@ package com.yggdrasil.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 import java.util.LinkedList;
@@ -46,24 +48,31 @@ public class CoreApplication {
 
   List<File> files = new LinkedList<>();
 
-	public static void main(String[] args) {
-		SpringApplication.run(CoreApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(CoreApplication.class, args);
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:8080", "http://localhost:5173");
+      }
+    };
+  }
 
   @GetMapping("/hello")
-  @CrossOrigin(origins = {"http://localhost:8080", "http://localhost:5173"})
   public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
     return String.format("Hello %s!", name);
   }
 
   @GetMapping("/files")
-  @CrossOrigin(origins = {"http://localhost:8080", "http://localhost:5173"})
   public List<File> listFiles() {
     return this.files;
   }
 
   @PostMapping("/files")
-  @CrossOrigin(origins = {"http://localhost:8080", "http://localhost:5173"})
   public ResponseEntity<Boolean> createFile(@RequestParam("file") MultipartFile file) throws IOException {
     File fileRecord = new File(this.files.size(), file);
     this.files.add(fileRecord);
@@ -71,32 +80,30 @@ public class CoreApplication {
   }
 
   @DeleteMapping("/files/{id}")
-  @CrossOrigin(origins = {"http://localhost:8080", "http://localhost:5173"})
   public ResponseEntity<Long> deleteFile(@PathVariable Long id) {
     Iterator<File> filesIterator = this.files.iterator();
 
     while (filesIterator.hasNext()) {
-        File file = filesIterator.next();
+      File file = filesIterator.next();
 
-        if (file.id == id)
-            filesIterator.remove();
+      if (file.id == id)
+        filesIterator.remove();
     }
 
     return new ResponseEntity<>(HttpStatus.NO_CONTENT);
   }
 
   @GetMapping("/files/{id}/download")
-  @CrossOrigin(origins = {"http://localhost:8080", "http://localhost:5173"})
   public ResponseEntity<byte[]> downloadFile(@PathVariable Long id) throws IOException {
     Iterator<File> filesIterator = this.files.iterator();
 
     while (filesIterator.hasNext()) {
-        File file = filesIterator.next();
+      File file = filesIterator.next();
 
-        if (file.id == id)
-            return ResponseEntity.ok()
-                .contentType(MediaType.valueOf(file.contentType))
-                .body(file.bytes);
+      if (file.id == id)
+        return ResponseEntity.ok()
+            .contentType(MediaType.valueOf(file.contentType))
+            .body(file.bytes);
     }
 
     return new ResponseEntity<>(HttpStatus.NOT_FOUND);
