1dc34f73d72d0cb2788ab8417a47a27d9f25e89f
none <none>
2023-12-31 00:30:44 -0500

    feat(files): add endpoint to download file

--- src/main/java/com/yggdrasil/core/CoreApplication.java
+++ src/main/java/com/yggdrasil/core/CoreApplication.java
@@ -6,37 +6,58 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 import java.util.List;
 import java.util.LinkedList;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.Serializable;
 
 class File implements Serializable {
   public int id;
   public String name;
+  public Long size;
 
-  public File(int id, String name) {
+  // @JsonSerialize(using = FileFileSerializer.class)
+  transient MultipartFile file;
+  transient byte[] bytes;
+
+  public File(int id, MultipartFile file) throws IOException {
     this.id = id;
-    this.name = name;
+
+    this.file = file;
+    this.name = file.getOriginalFilename();
+    this.size = file.getSize();
+    this.bytes= file.getBytes();
   }
+}
+
+class FileFileSerializer extends JsonSerializer<MultipartFile> {
+    @Override
+    public void serialize(MultipartFile file, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        jsonGenerator.writeString(file.getOriginalFilename());
+    }
 }
 
 @SpringBootApplication
 @RestController
 public class CoreApplication {
 
-  List<File> files = new LinkedList<>(List.of(
-    new File(1, "file-1.pdf"),
-    new File(2, "file-2.pdf"),
-    new File(3, "file-3.pdf")
-  ));
+  List<File> files = new LinkedList<>();
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoreApplication.class, args);
@@ -53,9 +74,10 @@ public class CoreApplication {
   }
 
   @PostMapping("/files")
-  public File createFile(@RequestBody File file) {
-    this.files.add(file);
-    return file;
+  public ResponseEntity<Boolean> createFile(@RequestParam("file") MultipartFile file) throws IOException {
+    File fileRecord = new File(this.files.size(), file);
+    this.files.add(fileRecord);
+    return new ResponseEntity<Boolean>(true, HttpStatus.CREATED);
   }
 
   @DeleteMapping("/files/{id}")
@@ -70,5 +92,19 @@ public class CoreApplication {
     }
 
     return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+  }
+
+  @GetMapping("/files/{id}/download")
+  public @ResponseBody byte[] downloadFile(@PathVariable Long id) throws IOException {
+    Iterator<File> filesIterator = this.files.iterator();
+
+    while (filesIterator.hasNext()) {
+        File file = filesIterator.next();
+
+        if (file.id == id)
+            return file.bytes;
+    }
+
+    throw new IOException();
   }
 }
