98c93d0849b819ff7ea9283fe69422ee2a82fe4f
none <none>
2023-12-31 02:45:07 -0500

    feat(files): allow all methods in all mappings in CORS

--- src/main/java/com/yggdrasil/core/CoreApplication.java
+++ src/main/java/com/yggdrasil/core/CoreApplication.java
@@ -57,7 +57,7 @@ public class CoreApplication {
     return new WebMvcConfigurer() {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:8080", "http://localhost:5173");
+        registry.addMapping("/**").allowedOrigins("http://localhost:8080", "http://localhost:5173").allowedMethods("*");
       }
     };
   }
