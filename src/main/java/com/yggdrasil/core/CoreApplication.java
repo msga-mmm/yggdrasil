2997dae69fa4729ed49bf71079e705d398e2fd7a
none <none>
2024-01-01 17:43:09 -0500

    feat(auth): add keycloak integration for authentication
    commit 33f9cb0267c38dbdf7c41236538b50bd22e5c887
    Author: none <none>
    Date:   Mon Jan 1 17:39:17 2024 -0500
    
        feat(files): add keycloak to the docker compose file
    
    commit 5ed56ff585340600a20e0a7f40fbb0bb0d7fb789
    Author: none <none>
    Date:   Mon Jan 1 03:28:57 2024 -0500
    
        feat(files): add keycloak integration for authentication

--- src/main/java/com/yggdrasil/core/CoreApplication.java
+++ src/main/java/com/yggdrasil/core/CoreApplication.java
@@ -1,5 +1,6 @@
 package com.yggdrasil.core;
 
+import org.keycloak.KeycloakPrincipal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
@@ -17,6 +23,8 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.LinkedList;
 import java.util.Iterator;
 import java.io.IOException;
@@ -27,12 +35,14 @@ class File implements Serializable {
   public String name;
   public Long size;
   public String contentType;
+  public String userID;
 
   transient MultipartFile file;
   transient byte[] bytes;
 
-  public File(int id, MultipartFile file) throws IOException {
+  public File(int id, MultipartFile file, String userID) throws IOException {
     this.id = id;
+    this.userID = userID;
 
     this.file = file;
     this.name = file.getOriginalFilename();
@@ -62,40 +72,58 @@ public class CoreApplication {
     };
   }
 
+  public List<File> getVisibleUserFiles(String userID) {
+    List<File> visibleUserFiles = this.files.stream().filter(file -> file.userID.equals(userID))
+        .collect(Collectors.toList());
+    return visibleUserFiles;
+  }
+
   @GetMapping("/hello")
   public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
     return String.format("Hello %s!", name);
   }
 
   @GetMapping("/files")
-  public List<File> listFiles() {
-    return this.files;
+  public List<File> listFiles(@AuthenticationPrincipal Jwt jwt) {
+    String userID = jwt.getClaimAsString("sub");
+    List<File> visibleUserFiles = this.getVisibleUserFiles(userID);
+
+    return visibleUserFiles;
   }
 
   @PostMapping("/files")
-  public ResponseEntity<File> createFile(@RequestParam("file") MultipartFile file) throws IOException {
-    File fileRecord = new File(this.files.size(), file);
+  public ResponseEntity<File> createFile(@RequestParam("file") MultipartFile file, @AuthenticationPrincipal Jwt jwt)
+      throws IOException {
+    String userID = jwt.getClaimAsString("sub");
+    File fileRecord = new File(this.files.size(), file, userID);
+
     this.files.add(fileRecord);
+
     return new ResponseEntity<File>(fileRecord, HttpStatus.CREATED);
   }
 
   @DeleteMapping("/files/{id}")
-  public ResponseEntity<Long> deleteFile(@PathVariable Long id) {
+  public ResponseEntity<Long> deleteFile(@PathVariable Long id, @AuthenticationPrincipal Jwt jwt) {
+    String userID = jwt.getClaimAsString("sub");
     Iterator<File> filesIterator = this.files.iterator();
 
     while (filesIterator.hasNext()) {
       File file = filesIterator.next();
 
-      if (file.id == id)
+      if (file.id == id && file.userID.equals(userID)) {
         filesIterator.remove();
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+      }
     }
 
-    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    return new ResponseEntity<>(HttpStatus.NOT_FOUND);
   }
 
   @GetMapping("/files/{id}/download")
-  public ResponseEntity<byte[]> downloadFile(@PathVariable Long id) throws IOException {
-    Iterator<File> filesIterator = this.files.iterator();
+  public ResponseEntity<byte[]> downloadFile(@PathVariable Long id, @AuthenticationPrincipal Jwt jwt) throws IOException {
+    String userID = jwt.getClaimAsString("sub");
+    List<File> visibleUserFiles = this.getVisibleUserFiles(userID);
+    Iterator<File> filesIterator = visibleUserFiles.iterator();
 
     while (filesIterator.hasNext()) {
       File file = filesIterator.next();
