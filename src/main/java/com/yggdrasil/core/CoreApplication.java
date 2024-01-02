201ad4e8489f79bd72c0fe1e3578e2daad040793
none <none>
2024-01-02 01:23:11 -0500

    chore(core): remove unnecessary annotations

--- src/main/java/com/yggdrasil/core/CoreApplication.java
+++ src/main/java/com/yggdrasil/core/CoreApplication.java
@@ -3,12 +3,10 @@ package com.yggdrasil.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@RestController
 public class CoreApplication {
   public static void main(String[] args) {
     SpringApplication.run(CoreApplication.class, args);
