4c5d19d2ec56fda89ac8da672b0149ee8c2e8b04
none <none>
2024-01-08 17:32:20 -0500

    feat(auth): migrate from react to angular port

--- src/main/java/com/yggdrasil/core/CoreApplication.java
+++ src/main/java/com/yggdrasil/core/CoreApplication.java
@@ -19,7 +19,7 @@ public class CoreApplication {
       public void addCorsMappings(CorsRegistry registry) {
         registry
             .addMapping("/**")
-            .allowedOrigins("http://localhost:8080", "http://localhost:5173")
+            .allowedOrigins("http://localhost:8080", "http://localhost:4200")
             .allowedMethods("*");
       }
     };
